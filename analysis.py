e67fcc4570ddf537e9bf5cc7352f12b10b493793
Євген <smotrock1@gmail.com>
2026-05-17 18:54:02 +0300

    add test comment1

--- analysis.py
+++ analysis.py
@@ -3,4 +3,5 @@ data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
 df = pd.DataFrame(data)
 print("Продажі по містах:")
 print(df)
+#test comment1
 print("Середнє значення:", df["sales"].mean())
